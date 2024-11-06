71f8432f3be4ef6db8142e43ed7f4b345ec1ea81
Cristian Guedes <cristian.edel@yahoo.com>
2024-11-06 19:20:42 -0300

    final

--- Lista-de-Tarefas/src/main/java/conexao/conexaobanco.java
+++ Lista-de-Tarefas/src/main/java/conexao/conexaobanco.java
@@ -4,19 +4,12 @@ import java.sql.DriverManager;
 
 public class conexaobanco {
 
-//	private Connection con;	
-//	private String DRIVER = "com.mysql.cj.jdbc.Driver";
-//    private static final String URL = System.getenv("DB_URL");
-//    private static final String USER = System.getenv("DB_USER");
-//    private static final String PASSWORD = System.getenv("DB_PASSWORD");
-	
 	private Connection con;	
 	private String DRIVER = "com.mysql.cj.jdbc.Driver";
-	private String URL = "jdbc:mysql://localhost:3306/Tarefas"; // TODO precisa verificar em outros computadores porque não é um banco online;
-	private String USER = "root";
-	private String PASSWORD = "Cc01102003+"; // TODO precisa verificar em outros computadores porque não é um banco
-												// online;
-
+	   private static final String URL = "jdbc:mysql://" + System.getenv("MYSQLHOST") + ":" + System.getenv("MYSQLPORT") + "/" + System.getenv("MYSQLDATABASE") + "?useSSL=false";
+	    private static final String USER = System.getenv("MYSQLUSER");
+	    private static final String PASSWORD = System.getenv("MYSQLPASSWORD");
+	
 	public Connection conexaologin() {
 		try {
 			Class.forName(DRIVER);
